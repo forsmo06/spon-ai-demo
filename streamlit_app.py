cb0259f5923b20259d08afda7156621e74e4ec78
forsmo06 <forsmo06@gmail.com>
2025-05-28 03:30:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,11 @@ st.header("🛠 Manuell kontroll (AI-styrt startverdi)")
 if beste_kombinasjon:
     ai_g105, ai_g106, ai_frisk, ai_prim, ai_trykk, ai_hombak, ai_maier, ai_fukt = beste_kombinasjon
 
-    temp_til = st.slider("Innløpstemp. (røykgass før tørk) °C", 250, 500, ai_g105)
-    temp_ut = st.slider("Utløpstemp. (røykgass etter tørk) °C", 100, 180, ai_g106)
-    friskluft = st.slider("Forbrenning av støv (%)", 0, 100, ai_frisk)
-    primluft = st.slider("Brenner ytelse (Primærluftsflekt) (%)", 0, 100, ai_prim)
-    trykkovn = st.slider("Brennkammer temp. (Nedre ovn trykk Pa)", -500, 0, ai_trykk)
+    temp_til = st.slider("G80GT105 – Innløpstemp. (røykgass før tørk) °C", 250, 500, ai_g105)
+    temp_ut = st.slider("G80GT106 – Utløpstemp. (røykgass etter tørk) °C", 100, 180, ai_g106)
+    friskluft = st.slider("GS5P101 – Forbrenning av støv (%)", 0, 100, ai_frisk)
+    primluft = st.slider("GS5F101 – Brenner ytelse (Primærluftsflekt) (%)", 0, 100, ai_prim)
+    trykkovn = st.slider("G80GP101 – Brennkammertemp. (Trykk nedre ovn Pa)", -500, 0, ai_trykk)
     hombak_mating = st.slider("Utmating Hombak (%)", 0, 100, ai_hombak)
     maier_mating = st.slider("Utmating Maier (%)", 0, 100, ai_maier)
 
@@ -82,8 +82,8 @@ st.header("🔧 Justering for ønsket fukt etter prøve")
 
 oensket_fukt = st.number_input("Ønsket fukt etter ny justering (%)", min_value=0.0, max_value=10.0, value=1.20, step=0.01)
 
-st.markdown("Angi dagens utløpstemp. og hvor mye du vil justere:")
-utlopstemp = st.number_input("Utløpstemp. (røykgass etter tørk) °C", min_value=100, max_value=200, value=140)
+st.markdown("Angi dagens G80GT106 og hvor mye du vil justere:")
+utlopstemp = st.number_input("G80GT106 – Utløpstemp. (røykgass etter tørk) °C", min_value=100, max_value=200, value=140)
 endring = st.slider("Still ned eller opp temp (grader)", -10, 10, step=1, value=0)
 
 ny_temp = utlopstemp + endring
